ee6a08b78c423ffbece5e511e1019608d5cfb417
Karen Seimears <karenqb@me.com>
2024-04-02 15:17:47 -0500

    Initial Commit

---
+++ WizardCahill2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WizardCahill2/WizardCahill2App.swift
@@ -0,0 +1,17 @@
+//
+//  WizardCahill2App.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WizardCahill2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
