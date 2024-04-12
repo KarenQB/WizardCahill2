a9e427dbf26cdc0d0da2af2bbfc024b86fd78afd
Karen Seimears <karenqb@me.com>
2024-04-12 13:24:16 -0500

    added topic categories

---
+++ WizardCahill2/LearnKeynote.swift
@@ -0,0 +1,18 @@
+//
+//  LearnKeynote.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/11/24.
+//
+
+import SwiftUI
+
+struct LearnKeynote: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnKeynote()
+}
---
+++ WizardCahill2/LearnPages.swift
@@ -0,0 +1,18 @@
+//
+//  LearnPages.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/11/24.
+//
+
+import SwiftUI
+
+struct LearnPages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnPages()
+}
