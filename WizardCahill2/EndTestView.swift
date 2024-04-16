7c7505248fef54be2edd3b335cda8098fd1d8766
Karen Seimears <karenqb@me.com>
2024-04-15 19:34:27 -0500

    Test view is all fixed up... working on making Learn play nice

---
+++ WizardCahill2/EndTestView.swift
@@ -0,0 +1,18 @@
+//
+//  EndTestView.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/15/24.
+//
+
+import SwiftUI
+
+struct EndTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EndTestView()
+}
