01353a0e9fff4b8cc7e77cc334be5415cefa379a
Karen Seimears <karenqb@me.com>
2024-04-16 16:04:53 -0500

    I've gone from feeling like I'm finally getting this ---- to wanting to retire forever in the past 6 hours. :)

---
+++ WizardCahill2/SubtitleView.swift
@@ -0,0 +1,18 @@
+//
+//  SubtitleView.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/16/24.
+//
+
+import SwiftUI
+
+struct SubtitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubtitleView()
+}
