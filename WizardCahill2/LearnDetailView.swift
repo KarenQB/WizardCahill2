f11570f5abe6ea60c092aa0787921026b97bd33a
Karen Seimears <karenqb@me.com>
2024-04-17 13:07:09 -0500

    updated directory

--- WizardCahill2/LearnDetailView.swift
+++ WizardCahill2/LearnDetailView.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 
 struct LearnDetailView: View {
-    var subtitle: String // Define a variable to hold the subtitle
+    var topic: Topics
+    var subtitles: [String]
+    let allTopics = Topics.allTopics
+    var learn: [Learn]
     
     @StateObject private var learnService = LearnService()
+
+    @State private var selectedSubtitleIndex = 0
+    @State private var filteredLearn: [Learn] = []
+    
+    var selectedSubtitle: String {
+        return subtitles[selectedSubtitleIndex]
+    }
     
     var body: some View {
         ZStack {
@@ -13,55 +23,88 @@ struct LearnDetailView: View {
                 .opacity(0.4)
             
             VStack {
-                // Use the provided subtitle variable instead of accessing .subtitle directly
-                switch subtitle {
-                case "Apps":
-                    Text(subtitle)
-                case "Control Center":
-                    Text(subtitle)
-                case "Spoken Text":
-                    Text(subtitle)
-                case "Notes":
-                    Text(subtitle)
-                default:
-                    Text("Content for \(subtitle) goes here")
+                VStack {
+                    Text(selectedSubtitle)
+                        .font(Font.custom("Truecat", size: 80))
+                    
+                    ForEach(filteredLearn, id: \.id) { learn in
+                        VStack(alignment: .leading) {
+                            Text(learn.title)
+                                .font(Font.custom("Truecat", size: 40))
+                                .padding()
+                            HStack {
+                                Text(learn.explore)
+                                
+                                Button(action: {
+                                    if let url = URL(string: learn.exploreURL) {
+                                        UIApplication.shared.open(url)
+                                    }
+                                }) {
+                                    Image(systemName: "wand.and.stars")
+                                }
+                            }
+                            .font(.title)
+                            .frame(width: 700)
+                            .padding()
+                            .cornerRadius(8)
+                        }
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Do This:")
+                                    .font(Font.custom("Truecat", size: 40))
+                                    .padding()
+                                ForEach(Array(learn.dos.enumerated()), id: \.1) { index, dos in
+                                    Text("\(index + 1). \(dos)")
+                                }
+                                .font(.title)
+                                .padding(.bottom, 5)
+                            }
+                            .padding()
+                            .cornerRadius(8)
+                            .padding()
+                            
+                            Spacer()
+                            
+                            Image("cahill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 400)
+                        }
+                        VStack {
+                            VStack(alignment: .leading) {
+                                Text("Extend it:")
+                                    .font(Font.custom("Truecat", size: 40))
+                                    .padding()
+                                ForEach(Array(learn.extends.enumerated()), id: \.1) { index, extends in
+                                    Text("🤔 \(extends)")
+                                }
+                                .font(.title)
+                                .padding(.bottom, 2)
+                            }
+                            .padding()
+                            .cornerRadius(8)
+                            .padding()
+                        }
+                    }
                 }
                 
-                // You can add more content specific to the subtitle here if needed
+                Spacer()
                 
-                // Example:
-                // Text("Some content specific to \(subtitle)")
+                VStack {
+                    Picker(selection: $selectedSubtitleIndex, label: Text("Select a topic to explore")) {
+                        ForEach(0..<subtitles.count, id: \.self) { index in
+                            Text(subtitles[index])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(50)
+                }
             }
-            .font(Font.custom("Truecat", size: 30))
-            
         }
         
-        
-        TabView {
-            LearnDetailView(subtitle: "Apps")
-                .tabItem {
-                    Label("Apps", systemImage: "apps.ipad")
-                }
-            LearnDetailView(subtitle: "Control Center")
-                .tabItem {
-                    Label("Control Center", systemImage: "gear")
-                }
-            LearnDetailView(subtitle: "Camera")
-                .tabItem {
-                    Label("Camera", systemImage: "camera")
-                }
-            LearnDetailView(subtitle: "Spoken Text")
-                .tabItem {
-                    Label("Spoken Text", systemImage: "speaker.wave.2.bubble.left.fill")
-                }
-            LearnDetailView(subtitle: "Notes")
-                .tabItem {
-                    Label("Notes", systemImage: "apps.ipad")
-                }
-            LearnDetailView(subtitle: "Translate")
-                .tabItem {
-                    Label("Translate", systemImage: "globe")
-                }
-        }
     }
+    func updateFilteredLearn() {
+            filteredLearn = learnService.learnBank.filter { $0.subtitle == selectedSubtitle }
+        print(SubtitleView(selectedTopic: .ipad, subtitles: []))
+        }
 }
--- WizardCahill2/LearnSubtitleView.swift
+++ WizardCahill2/LearnSubtitleView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct LearnSubtitleView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var learn: Learn
+    
+    @StateObject private var learnService = LearnService()
+    
+        var subtitle: String
+        var explore: String
+        var exploreURL: String
+        var dos: [String]
+        var extends: [String]
+        
+    let allTopics = Topics.allTopics
+    
+        var body: some View {
+            VStack {
+                Text(subtitle)
+                    .font(Font.custom("Truecat", size: 30))
+                Text(explore)
+                Link("Explore", destination: URL(string: exploreURL)!)
+                ForEach(dos, id: \.self) { item in
+                    Text(item)
+                }
+                ForEach(extends, id: \.self) { item in
+                    Text(item)
+                }
+            }
+        }
     }
-}
+let fakeLearn = Learn(title: "Fake", subtitle: "Fake Subtitle", explore: "Fake Explore", exploreURL: "https://fakeurl.com", dos: ["Fake Do 1", "Fake Do 2"], extends: ["Fake Extend 1", "Fake Extend 2"], topic: .ipad)
 
 #Preview {
-    LearnSubtitleView()
+    LearnSubtitleView(learn: fakeLearn, subtitle: "Fake Subtitle", explore: "Fake Explore", exploreURL: "https://fakeurl.com", dos: ["Fake Do 1", "Fake Do 2"], extends: ["Fake Extend 1", "Fake Extend 2"])
 }
+
+
