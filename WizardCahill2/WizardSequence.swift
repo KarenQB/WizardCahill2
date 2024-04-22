16f0ee2717625780662a4904819c34fce08f44b2
Karen Seimears <karenqb@me.com>
2024-04-22 11:44:58 -0500

    Everything just works now... need GIFs for learn pages... waiting on content to be written

---
+++ WizardCahill2/WizardSequence.swift
@@ -0,0 +1,18 @@
+//
+//  WizardSequence.swift
+//  WizardCahill2
+//
+//  Created by Karen Seimears on 4/18/24.
+//
+
+import SwiftUI
+
+struct WizardSequence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WizardSequence()
+}
